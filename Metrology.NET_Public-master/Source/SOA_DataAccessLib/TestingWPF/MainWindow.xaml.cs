38e460aaef435f84b10a38f7949db30d5d695a45
mSaeedi <mahdi.nikoo@gmail.com>
2019-07-15 12:53:45 +0300

    new comment added

--- Metrology.NET_Public-master/Source/SOA_DataAccessLib/TestingWPF/MainWindow.xaml.cs
+++ Metrology.NET_Public-master/Source/SOA_DataAccessLib/TestingWPF/MainWindow.xaml.cs
@@ -57,6 +57,7 @@ namespace TestingWPF
 
             // comment added
             //mahdi comment-updated
+            //new comment
             
 
             doc.Save(@"C:\Users\mckaya\Desktop\sample.xml");
