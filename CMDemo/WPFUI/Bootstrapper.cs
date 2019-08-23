293f880384392ac63310bdbb17dd407490319863
mckaya <mckaya@ceng.metu.edu.tr>
2019-08-23 18:10:31 +0300

    CMDemo solution is added

---
+++ CMDemo/WPFUI/Bootstrapper.cs
@@ -0,0 +1,25 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WPFUI.ViewModels;
+
+namespace WPFUI
+{
+    public class Bootstrapper : BootstrapperBase // using Caliburn.Micro // must be added
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>(); // we want this code to run on startup // using WPFUI.ViewModels; must be added
+        }
+
+    }
+}
---
+++ CMDemo/WPFUI/ViewModels/ShellViewModel.cs
@@ -0,0 +1,83 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFUI.Models;
+
+namespace WPFUI.ViewModels
+{
+    public class ShellViewModel : Screen // keyboard shortcut: ctrl + .
+    {
+        private string _firstName;
+        private string _lastName;
+        private BindableCollection<PersonModel> _people = new BindableCollection<PersonModel>();
+        private PersonModel _selectedPerson;
+
+        public ShellViewModel()
+        {
+            People.Add(new PersonModel { FirstName = "MC", LastName = "Kaya" });
+            People.Add(new PersonModel { FirstName = "Mahdi", LastName = "SN" });
+            People.Add(new PersonModel { FirstName = "Moose", LastName = "Pi" });
+        }
+
+
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                _firstName = value;
+                NotifyOfPropertyChange(() => FirstName);
+                NotifyOfPropertyChange(() => FullName);
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+                NotifyOfPropertyChange(() => LastName);
+                NotifyOfPropertyChange(() => FullName);
+            }
+        }
+       
+        public string FullName
+        {
+            get
+            {
+                return $"{ FirstName } { LastName }";
+            }
+        }
+
+       
+
+        public BindableCollection<PersonModel> People
+        {
+            get { return _people; }
+            set { _people = value; }
+        }
+
+
+
+        public PersonModel SelectedPerson
+        {
+            get { return _selectedPerson; }
+            set
+            {
+                _selectedPerson = value;
+                NotifyOfPropertyChange(() => SelectedPerson);
+            }
+        }
+
+    }
+}
